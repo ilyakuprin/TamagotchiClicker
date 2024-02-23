726e1cf160100167dc210dfc738e71fe28adb0e8
ilyakuprin <ilia6478@gmail.com>
2024-02-23 17:41:58 +0300

    AdvertisingAwardConfig change max value field
    Увеличение максимального времени действия х2 за рекламу

--- Assets/_TamagotchiClicker/Scripts/ScriptableObject/AdvertisingAwardConfig.cs
+++ Assets/_TamagotchiClicker/Scripts/ScriptableObject/AdvertisingAwardConfig.cs
@@ -5,6 +5,6 @@ namespace TamagotchiClicker
     [CreateAssetMenu(fileName = "AdvertisingAwardConfig", menuName = "Configs/AdvertisingAwardConfig")]
     public class AdvertisingAwardConfig : ScriptableObject
     {
-        [field: SerializeField, Range(0f, 30f)] public float MultiplierTime { get; private set; }
+        [field: SerializeField, Range(0f, 120f)] public float MultiplierTime { get; private set; }
     }
 }
